7f85da308c4f866d863a53f1e931d830cf2a597e
yusaristark <yusaristark@gmail.com>
2021-10-03 21:15:54 +0300

    redurant inports deleted

--- src/com/gridnine/testing/Main.java
+++ src/com/gridnine/testing/Main.java
@@ -1,9 +1,7 @@
 package com.gridnine.testing;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
